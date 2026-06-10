f08192b01009a700803db5fa8ae5daef50f08d6b
Yaniv Mizrahi <yanivmiz77@gmail.com>
2026-06-10 16:09:26 +0300

    Fix GitHub Pages routing and asset paths

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/stats-question-generator">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
@@ -25,3 +25,4 @@ const App = () => (
 );
 
 export default App;
+
